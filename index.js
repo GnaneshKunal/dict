a06604ab25c133fc1afe2c7145bf0de779f1e738
gnaneshkunal <gnaneshpingu@gmail.com>
2017-07-03 19:08:59 +0530

    removing morgan

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const express = require('express');
 const expressGraphQL = require('express-graphql');
-const morgan = require('morgan');
 const app = express();
 const server = http.createServer(app);
 const schema = require('./schema/schema');
@@ -10,7 +9,6 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(morgan('dev'));
 app.use('/graphql', expressGraphQL({
     schema,
     graphiql: true
