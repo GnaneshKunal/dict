65fb9624b2617826c901a3c7d5d3526583de7822
gnaneshkunal <gnaneshpingu@gmail.com>
2017-07-05 10:32:36 +0530

    separating rootQuery

---
+++ schema/rootQuery.js
@@ -0,0 +1,49 @@
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLNonNull, GraphQLString , GraphQLList } = graphql;
+const { getWord, getExamples } = require('../src/data/');
+const wordExample = require('./wordExampleType');
+const axios = require('axios');
+
+const WordType = require('./wordType');
+
+const queryType = new GraphQLObjectType({
+    name: 'QueryType',
+    description: 'The roor Query type',
+    fields: {
+        words: {
+            type: new GraphQLList(WordType),
+            resolve: () => getWord
+        },
+        word: {
+            type: new GraphQLList(WordType),
+            args: {
+                word: {
+                    type: new GraphQLNonNull(GraphQLString),
+                    description: 'The word you want to search',
+                }
+            },
+            resolve: (_, args) => {
+                return axios.get(`http://api.wordnik.com/v4/word.json/${args.word}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=${process.env.WORDNIK_API_KEY}`)
+                    .then(res => res.data)
+                    .catch(err => err);
+            }
+        },
+        examples: {
+            type: new GraphQLList(wordExample),
+            resolve: () => getExamples
+        },
+        str: {
+            type: new GraphQLList(GraphQLString),
+            resolve: () => ['sadsda', 'asdsda']
+        },
+        // str2: {
+        //     type: wordExample, // dude check this
+        //     resolve: () => {
+        //         return {
+        //             examples: ['asdsad', 'saddsadsa'] // nesting is what you were suffering
+        //         };
+        //     }
+        // }
+    }
+});
+module.exports = queryType;
