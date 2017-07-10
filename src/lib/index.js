380c569a8265ae77fb887aa89db43636d5ea9c9c
gnaneshkunal <gnaneshpingu@gmail.com>
2017-07-10 22:21:01 +0530

    addition static data

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,5 @@
 exports.wordrelationshipTypes = ['synonym', 'antonym', 'variant', 'equivalent', 'cross-reference', 'related-word', 'rhyme', 'form', 'etymologically-related-term', 'hypernym', 'hyponym', 'inflected-form', 'primary', 'same-context', 'verb-form', 'verb-stem'];
 exports.sourceDictionary = ['ahd', 'century', 'cmu', 'macmillan', 'wiktionary', 'webster', 'wordnet'];
 exports.typeFormat= ['ahd', 'arpabet', 'gcide-diacritical', 'IPA'];
+exports.definitionsSourceDictionary = ['all', 'ahd', 'century', 'wiktionary', 'webster', 'wordnet'];
+exports.partOfSpeech = ['noun', 'adjective', 'verb', 'adverb', 'interjection', 'pronoun', 'preposition', 'abbreviation', 'affix', 'article', 'auxiliary-verb', 'conjunction', 'definite-article', 'family-name', 'given-name', 'idiom', 'imperative', 'noun-plural', 'noun-posessive', 'past-participle', 'phrasal-prefix', 'proper-noun', 'proper-noun-plural', 'proper-noun-posessive', 'suffix', 'verb-intransitive', 'verb-transitive'];
