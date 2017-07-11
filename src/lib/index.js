ecb99e82188e1d2752a0af8162a68efb314e7a2f
gnaneshkunal <gnaneshpingu@gmail.com>
2017-07-11 14:42:44 +0530

    hyphenations' dictionary

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,3 +3,4 @@ exports.sourceDictionary = ['ahd', 'century', 'cmu', 'macmillan', 'wiktionary', 
 exports.typeFormat= ['ahd', 'arpabet', 'gcide-diacritical', 'IPA'];
 exports.definitionsSourceDictionary = ['all', 'ahd', 'century', 'wiktionary', 'webster', 'wordnet'];
 exports.partOfSpeech = ['noun', 'adjective', 'verb', 'adverb', 'interjection', 'pronoun', 'preposition', 'abbreviation', 'affix', 'article', 'auxiliary-verb', 'conjunction', 'definite-article', 'family-name', 'given-name', 'idiom', 'imperative', 'noun-plural', 'noun-posessive', 'past-participle', 'phrasal-prefix', 'proper-noun', 'proper-noun-plural', 'proper-noun-posessive', 'suffix', 'verb-intransitive', 'verb-transitive'];
+exports.hyphenationsSourceDictionary = ['ahd', 'century', 'wikitionary', 'webster', 'wordnet'];
