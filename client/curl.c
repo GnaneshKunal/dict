6f7d0fced9b82829105db47509c408ad978859e6
gnaneshkunal <gnaneshpingu@gmail.com>
2017-07-16 14:17:03 +0530

    json-c integration

--- client/curl.c
+++ client/curl.c
@@ -4,6 +4,7 @@
 
 #include "dict.h"
 #include <curl/curl.h>
+#include <json-c/json.h>
 
 static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream) {
     size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
@@ -82,6 +83,38 @@ int main(int argc, char **argv) {
 
     /* cleanup curl stuff */
     curl_easy_cleanup(curl_handle);
+
+    struct json_object *jobj;
+    const char *question = "Mum, clouds hide alien spaceships don't they ?";
+    const char *answer = "Of course not! (\"sigh\")";
+    int i;
+    struct {
+        int flag;
+        const char *flag_str;
+    } json_flags[] = {{JSON_C_TO_STRING_PLAIN, "JSON_C_TO_STRING_PLAIN"},
+                      {JSON_C_TO_STRING_SPACED, "JSON_C_TO_STRING_SPACED"},
+                      {JSON_C_TO_STRING_PRETTY, "JSON_C_TO_STRING_PRETTY"},
+                      {JSON_C_TO_STRING_NOZERO, "JSON_C_TO_STRING_NOZERO"},
+                      {JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY,
+                       "JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY"},
+                      {-1, NULL}}; // Create an anonymous struct, instanciate an array and fill it
+
+    printf("Using printf(): \"%s\", \"%s\"\n\n", question, answer);
+    printf("Using json_object_to_json_string_ext():\n");
+
+    /*
+     * The following create an object and add the question and answer to it.
+     */
+    jobj = json_object_new_object();
+    json_object_object_add(jobj, "question", json_object_new_string(question));
+    json_object_object_add(jobj, "answer", json_object_new_string(answer));
+
+    for (i = 0; json_flags[i].flag_str != NULL; i++) {
+        printf("\nFlag %s:\n---\n", json_flags[i].flag_str);
+        printf("%s\n---\n", json_object_to_json_string_ext(jobj, json_flags[i].flag));
+    }
+
+    json_object_put(jobj); // Delete the json object
 
     return 0;
 }
