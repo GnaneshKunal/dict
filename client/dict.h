962db2c14e2e44f0e5d96a13fb458bc171cfb2ad
gnaneshkunal <gnaneshpingu@gmail.com>
2017-07-20 22:03:49 +0530

    adding and declaring headers & functions

--- client/dict.h
+++ client/dict.h
@@ -1,1 +1,16 @@
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void print_usage(void);
+bool argv_exists(char *);
 char *parser(char *, char *, char *, int, ...);
+
+typedef struct wordDefinition {
+    int partOfSpeech;
+    int text;
+    int sequence;
+} wordDefinition;
