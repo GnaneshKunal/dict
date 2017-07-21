332be3520bd7febbb9681de948ad0f77fcb8d97f
gnaneshkunal <gnaneshpingu@gmail.com>
2017-07-21 22:01:51 +0530

    exit()

--- client/dict.c
+++ client/dict.c
@@ -2,13 +2,15 @@
 
 int main(int argc, char **argv) {
     int i;
-    if (argc != 2) {
+    if (argc < 2) {
         print_usage();
+        exit(0);
     }
 
     if (argv_exists(argv[1]) == false) {
-        printf("dict error: Undefined Options\n");
+        printf("dict error: Undefined Option\n");
         print_usage();
+        exit(1);
     }
     exit(0);
 }
