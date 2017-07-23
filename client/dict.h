26b951382a954367e531ec5533836705d31d2c59
gnaneshkunal <gnaneshpingu@gmail.com>
2017-07-23 14:09:43 +0530

    removing typedefs, extern structures, and defining make_

--- client/dict.h
+++ client/dict.h
@@ -7,64 +7,13 @@
 
 void print_usage(void);
 bool argv_exists(char *);
-char *parser(char *, char *, char *, int, ...);
+void make_process(char **, char *);
+int request(char *, char *);
 
-typedef struct ar_options {
-    const char *argv_name;
-    const char *argv_value;
-} ar_options;
-
-extern ar_options limit;
-extern ar_options partOfSpeech;
-extern ar_options includeRelated;
-extern ar_options sourceDictionaries;
-extern ar_options useCanonical;
-extern ar_options includeTags;
-extern ar_options includeDuplicates;
-extern ar_options skip;
-extern ar_options sourceDictionary;
-extern ar_options wlmi;
-extern ar_options typeFormat;
-extern ar_options relationshipTypes;
-extern ar_options limitPerRelationshipType;
-
-typedef struct wordDefinition {
-    ar_options limit;
-    ar_options partOfSpeech;
-    ar_options includeRelated;
-    ar_options sourceDictionaries;
-    ar_options useCanonical;
-    ar_options includeTags;
-} wordDefinition;
-
-typedef struct wordExample {
-    ar_options includeDuplicates;
-    ar_options useCanonical;
-    ar_options skip;
-    ar_options limit;
-} wordExample;
-
-typedef struct wordHyphenation {
-    ar_options useCanonical;
-    ar_options sourceDictionary;
-    ar_options limit;
-} wordHyphenation;
-
-typedef struct wordPhrase {
-    ar_options useCanonical;
-    ar_options wlmi;
-    ar_options limit;
-} wordPhrase;
-
-typedef struct wordPronunciation {
-    ar_options useCanonical;
-    ar_options limit;
-    ar_options sourceDictionary;
-    ar_options typeFormat;
-} wordPronunciation;
-
-typedef struct wordRelated {
-    ar_options useCanonical;
-    ar_options relationshipTypes;
-    ar_options limitPerRelationshipType;
-} wordRelated;
+void make_word(char **, char *);
+void make_examples(char **, char *);
+void make_hyphenations(char **, char *);
+void make_pronunication(char **, char *);
+void make_phrases(char **, char *);
+void make_relations(char **, char *);
+void make_wod(char *);
