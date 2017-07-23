675beae670b27890ddb84fb408c20b5aa152bc3a
gnaneshkunal <gnaneshpingu@gmail.com>
2017-07-23 14:10:17 +0530

    rm puts && dat

--- client/request.c
+++ client/request.c
@@ -2,7 +2,6 @@
 #include <curl/curl.h>
 
 int request(char *query, char *response) {
-    puts(query);
     CURL *curl;
     CURLcode res;
     curl = curl_easy_init();
@@ -10,10 +9,8 @@ int request(char *query, char *response) {
         curl_easy_setopt(curl, CURLOPT_URL, "http://dict-cli.herokuapp.com/graphql");
         struct curl_slist *headers = NULL;
         headers = curl_slist_append(headers, "Content-Type: application/graphql");
-        char *dat = query;
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, dat);
-
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, query);
         res = curl_easy_perform(curl);
         if (res != CURLE_OK)
             strcpy(response, curl_easy_strerror(res));
