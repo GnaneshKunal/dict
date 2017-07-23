af8cec21fc1003ab5f3f9ff4f65d93e63d5ef0fa
gnaneshkunal <gnaneshpingu@gmail.com>
2017-07-23 17:01:44 +0530

    ANSI color codes

--- client/dict.h
+++ client/dict.h
@@ -5,6 +5,14 @@
 #include <string.h>
 #include <unistd.h>
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 void print_usage(void);
 bool argv_exists(char *);
 void make_process(char **, char *);
