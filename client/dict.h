842c3fe4bc499ba6aa6bb1d13c79b816ff435b93
gnaneshkunal <gnaneshpingu@gmail.com>
2017-07-25 16:47:47 +0530

    adding function headers

--- client/dict.h
+++ client/dict.h
@@ -5,13 +5,13 @@
 #include <string.h>
 #include <unistd.h>
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
 void print_usage(void);
 bool argv_exists(char *);
@@ -25,3 +25,12 @@ void make_pronunication(char **, char *);
 void make_phrases(char **, char *);
 void make_relations(char **, char *);
 void make_wod(char *);
+
+void rem_array(char *);
+void parse_word(char *, char *);
+void parse_examples(char *, char *);
+void parse_related(char *, char *);
+void parse_pronunciation(char *, char *);
+void parse_hyphenation(char *, char *);
+void parse_phrases(char *, char *);
+void parse_wotd(char *, char *);
