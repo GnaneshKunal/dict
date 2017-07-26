f7c4251e6cda9ccd2e933dc279fd40dd3c43c6a1
gnaneshkunal <gnaneshpingu@gmail.com>
2017-07-26 17:23:18 +0530

    other options

--- client/util.c
+++ client/util.c
@@ -12,6 +12,20 @@ void print_usage(void) {
     printf("\t-o\t: Get word Pronunciation\n");
     printf("\t-r\t: Get word Relations\n");
     printf("\t-x\t: Get word of the day\n");
+    printf("Other options:\n");
+    printf(
+    "\t-l={n}\t: Limit the result[definitions, examples, hyphenations, phrases pronunciations]\n");
+    printf("\t-uc={b}\t:  Use canonical[all]\n");
+    printf("\t-pos={s}\t: PartOfSpeech[definitions]\n");
+    printf("\t-sds={s}\t: SourceDictionaries[definitions]\n");
+    printf("\t-it={b}\t:  Include Tags[definitions]\n");
+    printf("\t-id={b}\t:  Include Duplicates[examples]\n");
+    printf("\t-s={n}\t:   Skip[examples]\n");
+    printf("\t-sd={s}\t:  SourceDictionary[hyphenations, pronunciations]\n");
+    printf("\t-tf={s}\t:  Type format[pronunciations]\n");
+    printf("\t-wlmi={s}\t:    wlmi[phrases]\n");
+    printf("\t-lrt={s}\t: limitPerRelationshipType[relations]\n");
+    printf("\t-rt={s}\t: Relationship Types\n");
 }
 
 bool argv_exists(char *str) {
